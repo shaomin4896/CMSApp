fe30021f5744191c6866ccb945f5c990789d2224
shaomin <sam880819@gmail.com>
2023-05-23 23:38:24 +0800

    Release: Last page ,In Progress : Self.razor

--- Models/PatientSelfHistory.cs
+++ Models/PatientSelfHistory.cs
@@ -8,12 +8,12 @@ namespace CaseManagementAPI.Models
     public class PatientSelfHistory
     {
         public int Id { get; set; }
-        public double Height { get; set; }
-        public double Weight { get; set; }
-        public string MedicineRecord { get; set; }
-        public int FootCare { get; set; }
-        public int SportCare { get; set; }
-        public int MouseCare { get; set; }
+        public double? Height { get; set; }
+        public double? Weight { get; set; }
+        public string? MedicineRecord { get; set; }
+        public int? FootCare { get; set; }
+        public int? SportCare { get; set; }
+        public int? MouseCare { get; set; }
         public BloodPressureTest BloodPressureTest { get; set; }
         public FootTest FootTest { get; set; }
         public BloodTest BloodTest { get; set; }
